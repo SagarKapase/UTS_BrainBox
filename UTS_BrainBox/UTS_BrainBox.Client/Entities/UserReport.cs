63c5917ee3df21291c243e68a9c32c19ac47a514
SagarKapase <sagarkapase139@gmail.com>
2025-07-15 19:39:26 +0530

    added userReports Page

---
+++ UTS_BrainBox/UTS_BrainBox.Client/Entities/UserReport.cs
@@ -0,0 +1,11 @@
+﻿namespace UTS_BrainBox.Client.Entities
+{
+    public class UserReport
+    {
+        public string EmployeeID { get; set; } = string.Empty;
+        public string EmployeeName { get; set; } = string.Empty;
+        public string Quiz { get; set; } = string.Empty;
+        public string Score {  get; set; } = string.Empty;
+        public string Date {  get; set; } = string.Empty;
+    }
+}
--- UTS_BrainBox/UTS_BrainBox/obj/Debug/net9.0/UTS_BrainBox.AssemblyInfo.cs
+++ UTS_BrainBox/UTS_BrainBox/obj/Debug/net9.0/UTS_BrainBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTS_BrainBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9fcf5294040eae6266b8b902933c99e0a298d92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85198a35d4d239dd0d987b7f2221c0a5561d5322")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTS_BrainBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTS_BrainBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
