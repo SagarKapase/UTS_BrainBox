d4b245c7068902937f4bb715bbf9d9aa5f55ae31
SagarKapase <sagarkapase139@gmail.com>
2025-07-19 15:37:44 +0530

    admin login from db and added snackbars

--- UTS_BrainBox/UTS_BrainBox.Client/obj/Debug/net9.0/UTS_BrainBox.Client.AssemblyInfo.cs
+++ UTS_BrainBox/UTS_BrainBox.Client/obj/Debug/net9.0/UTS_BrainBox.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTS_BrainBox.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0304081814ca0abc17f8298dacbc244272768efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8b8e892e53d031db8d53851a4c375d624564ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTS_BrainBox.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTS_BrainBox.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
