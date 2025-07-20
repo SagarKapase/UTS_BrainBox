46ee914e34b046a365648474584f793fbdf16d78
SagarKapase <sagarkapase139@gmail.com>
2025-07-20 10:38:32 +0530

    added quiz count on card from db also added snackbar on create quiz

--- UTS_BrainBox/UTS_BrainBox/obj/Debug/net9.0/UTS_BrainBox.AssemblyInfo.cs
+++ UTS_BrainBox/UTS_BrainBox/obj/Debug/net9.0/UTS_BrainBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UTS_BrainBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4b245c7068902937f4bb715bbf9d9aa5f55ae31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a8f27e3da3a3e6f28ec8a0ccdab150d5ce32a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("UTS_BrainBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UTS_BrainBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
